0a4d1fb15bfabbf7db8e5f20744cf001e830248f
John Bales <jrbdeveloper@gmail.com>
2020-01-01 09:55:50 -0800

    Create Autonomous.ino
    Initial commit

---
+++ Autonomous.ino
@@ -0,0 +1,83 @@
+#include <Maneuver.h>
+#include <Servo.h>
+
+Servo frontServo; // create servo object to control servo
+Maneuver maneuver;
+
+struct Sensor {
+  int Echo;
+  int Trig;
+} frontSensor, rearSensor;
+
+int rightDistance = 0, leftDistance = 0, frontDistance = 0, rearDistance = 0;
+
+//Ultrasonic distance measurement Sub function
+int getDistance(Sensor sensor) {
+  digitalWrite(sensor.Trig, LOW);   
+  delayMicroseconds(2);
+  digitalWrite(sensor.Trig, HIGH);  
+  delayMicroseconds(20);
+  digitalWrite(sensor.Trig, LOW);   
+  float Fdistance = pulseIn(sensor.Echo, HIGH);  
+  Fdistance = Fdistance / 58;
+  
+  return (int)Fdistance;
+}  
+
+void scan(Sensor sensor){
+  delay(1000);
+  maneuver.Turn('L',90);
+  leftDistance = getDistance(sensor);  
+  
+  delay(1000);
+  maneuver.Turn('R',180);
+  rightDistance = getDistance(sensor);
+  
+  delay(1000);
+  maneuver.Turn('L',90);
+  frontDistance = getDistance(sensor);
+  
+  setDirection();
+}
+
+void setDirection(){
+  if((rightDistance <= 50) && (leftDistance <= 50)){
+    maneuver.Reverse();
+    scan(frontSensor);    
+  }else if(rightDistance == leftDistance){
+    maneuver.Reverse();
+    scan(frontSensor);
+  }else if(leftDistance > rightDistance){
+    maneuver.Turn('L',90);
+  }else if (rightDistance > leftDistance){
+    maneuver.Turn('R', 90);
+  }else {
+    // write a function to randomly determine a direction here
+    maneuver.Turn('L',90);
+  }
+}
+
+void setup() { 
+  Serial.begin(9600); 
+
+  //frontServo.attach(3,700,2400);  // attach servo on pin 3 to servo object
+  maneuver.Configure({5, 6, 7, 8, 9, 11, false});
+
+  frontSensor.Echo = A4;
+  frontSensor.Trig = A5;
+  
+  pinMode(frontSensor.Echo, INPUT);    
+  pinMode(frontSensor.Trig, OUTPUT);  
+    
+  maneuver.Stop();
+}
+
+void loop() {
+  frontDistance = getDistance(frontSensor);
+  
+  if(frontDistance > 50){
+    maneuver.Forward();
+  }else {
+    scan(frontSensor);
+  } 
+}
