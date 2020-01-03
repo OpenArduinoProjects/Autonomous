588bb67803f23d2cefb73ce2e100296635a03788
John Bales <jrbdeveloper@gmail.com>
2020-01-02 17:52:50 -0800

    Update Autonomous.ino
    reverting some code that I didn't getting the results I was expecting from

--- Autonomous.ino
+++ Autonomous.ino
@@ -8,56 +8,41 @@ Sensor rearSensor;
 
 int rightDistance = 0, leftDistance = 0, frontDistance = 0, rearDistance = 0;
 
-void scan(Sensor sensor){
+void scan(){
   maneuver.Turn('L',90);
-  delay(200);
-  leftDistance = maneuver.Distance(sensor);  
-  delay(800);
+  leftDistance = maneuver.Distance(frontSensor);  
+  delay(1000);
   
   maneuver.Turn('R',180);
-  delay(200);
-  rightDistance = maneuver.Distance(sensor);
-  delay(800);
+  rightDistance = maneuver.Distance(frontSensor);
+  delay(1000);
   
   maneuver.Turn('L',90);
-  delay(200);
-  frontDistance = maneuver.Distance(sensor);
-  delay(800);
+  frontDistance = maneuver.Distance(frontSensor);
+  delay(1000);
 }
 
 void setDirection(){
   if((rightDistance <= 50) && (leftDistance <= 50) && (frontDistance <= 50)){
     maneuver.Backward();
     delay(400);
-    scan(frontSensor);
+    scan();
     setDirection();    
-  }
-  
-  if(rightDistance == leftDistance){
+  }else if(rightDistance == leftDistance){
     maneuver.Backward();
     delay(400);
-    scan(frontSensor);
+    scan();
     setDirection();
-  }
-
-  if(leftDistance > rightDistance){
+  }else if((leftDistance > 50) && (leftDistance > rightDistance)){
     maneuver.Turn('L',90);
-  }else if (rightDistance > leftDistance){
+  }else if ((rightDistance > 50) && (rightDistance > leftDistance)){
     maneuver.Turn('R', 90);
-  }else if ((frontDistance > leftDistance) && (frontDistance > rightDistance)) {
+  }else if((frontDistance > 50) && ((frontDistance > leftDistance) && (frontDistance > rightDistance))) {
     maneuver.Forward();
   }else {
     // write a function to randomly determine a direction here
     maneuver.Turn('L',90);
   }
-}
-
-void configureSensor(Sensor &sensor, int echo, int trig){
-  sensor.Echo = echo;
-  sensor.Trig = trig;
-  
-  pinMode(sensor.Echo, INPUT);    
-  pinMode(sensor.Trig, OUTPUT);
 }
 
 void setup() {
@@ -66,8 +51,12 @@ void setup() {
   //servo.attach(3,700,2400);  // attach servo on pin 3 to servo object
   maneuver.Configure({5, 6, 7, 8, 9, 11, false});
   maneuver.SetSpeed(250);
+
+  frontSensor.Echo = A4;
+  frontSensor.Trig = A5;
   
-  configureSensor(frontSensor, A4, A5); 
+  pinMode(frontSensor.Echo, INPUT);    
+  pinMode(frontSensor.Trig, OUTPUT);
     
   maneuver.Stop();
 }
@@ -78,7 +67,7 @@ void loop() {
   if(frontDistance > 50){
     maneuver.Forward();
   }else {
-    scan(frontSensor);
+    scan();
     setDirection();
   } 
 }
