ef580e9acc7d86e3bad0c4d6b7bb5c9a2aa0bfaf
John Bales <jrbdeveloper@gmail.com>
2020-01-21 20:50:58 -0800

    Update Autonomous.ino
    Writing messages to the LCD regarding distance measurements

--- Autonomous.ino
+++ Autonomous.ino
@@ -1,7 +1,5 @@
 #include <Maneuver.h>
-#include <Servo.h>
 
-Servo servo; // create servo object to control servo
 Maneuver maneuver;
 Sensor frontSensor;
 Sensor rearSensor;
@@ -11,7 +9,6 @@ int forwardDistance = 0;
 void setup() {
   Serial.begin(9600); 
 
-  //servo.attach(3,700,2400);  // attach servo on pin 3 to servo object
   maneuver.Configure({5, 6, 7, 8, 9, 11, true});
   maneuver.SetSpeed(250);
 
